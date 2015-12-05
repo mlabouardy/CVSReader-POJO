094ee2b5f55ba3a9171fe6e87f0b17c9c8f9ed5e
mlabouardy <mohamed.labouardy@gmail.com>
2015-12-05 18:02:29 +0100

    custom server

---
+++ constants.js
@@ -0,0 +1,5 @@
+'use strict'
+
+module.exports= Object.freeze({
+	LENGTH_ACCEPTED:40  //server accept only array of size 40 elements
+});
--- index.js
+++ index.js
@@ -1,17 +1,47 @@
 var express=require('express');
 var bodyParser = require('body-parser');
-
+var constants=require('./constants.js');
 var app=express();
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true})); 
 
+/*
+* This function generates random 
+* integer between two numbers low (inclusive) and high (inclusive)
+*/
+function randomIntInc (low, high) {
+    return Math.floor(Math.random() * (high - low + 1) + low);
+}
+
+/*
+* send error code according to data
+* length or availability of server
+*/
+function check(data, res){
+	var size=data.length;
+	var magic=randomIntInc(0,1);
+	if(size>constants.LENGTH_ACCEPTED){
+		res.status(400);
+		res.send('None shall pass');
+	}else if(magic==0){
+		res.status(503);
+		res.send('Service unavailable');
+	}else{
+		res.status(200);
+		res.send(data);
+	}
+}
+
 app.post('/orders',function(req,res){
-	res.send(req.body);
+	var orders=req.body;
+	check(orders, res);
 });
 
 app.post('/customers',function(req,res){
-	res.send(req.body);
+	var customers=req.body;
+	check(customers, res);
 });
 
 app.listen(3000,function(){
